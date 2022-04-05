d9a2aa98b843761f4f5d604c687c562f26792aa6
unknown <rehimov.xaliq@internet.ru>
2022-04-05 17:18:38 +0400

    Some additions

--- e-commerce project/static/js/password.js
+++
@@ -1,8 +0,0 @@
-const buttoneye = document.querySelector<HTMLElement>(".js-button-eye");
-const input = document.querySelector<HTMLElement>(".js-input");
-
-buttoneye.addEventListener("click", () => {
-    const visible = input.dataset.visible == "true";
-    input.dataset.visible = visible ? "false" : "true";
-    input.setAttribute("type", visible ? "password1" : "text");
-});
