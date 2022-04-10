636624252c6517aae7b8adbd82124e8db4442b65
unknown <rehimov.xaliq@internet.ru>
2022-04-10 20:59:12 +0400

    Pagenation work was done on the blog template

--- e-commerce project/Blog/views.py
+++ e-commerce project/Blog/views.py
@@ -1,15 +1,40 @@
+import math
 from django.shortcuts import render
 from Blog.models import Blog
 
 # Create your views here.
 
 def blog(request):
-    blogs = Blog.objects.all()
+    page = request.GET.get('page')
+    if page:
+        page = int(page)
+    else:
+        page = 1
+    per_count = 2
+    all_blogs = Blog.objects.all().count()
+    blogs = Blog.objects.all()[per_count * (page -1):(page * per_count)]
+    total_page_count = math.ceil(all_blogs / per_count)
+    previous_page = page - 1 if not page == 1 else page
+    next_page = page + 1 if not page == total_page_count else page
+    page_range = range(1, total_page_count + 1)
 
-    return render(request, 'blog.html', {'blogs':blogs})
+    current_page = page
+
+    #blogs = Blog.objects.all()
+
+    context = {
+        'blogs':blogs,
+        'current_page': current_page,
+        'previous_page': previous_page,
+        'next_page': next_page,
+        'page_range': page_range,
+
+    }
+
+    return render(request, 'blog.html', context)
 
 def blog_detail(request, slug):
 
-    blog = Blog.objects.filter(slug=slug)
+    blog = Blog.objects.filter(slug=slug).first()
 
     return render(request, 'single-blog.html', {'blog': blog})
