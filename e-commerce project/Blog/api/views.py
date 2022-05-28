d95a14aaeac6ddca75fbd78fe199b183e0f8b2bc
unknown <rehimov.xaliq@internet.ru>
2022-05-28 21:26:00 +0400

    l applied Django Rest Framework in MVT structure and some additions

--- e-commerce project/Blog/api/views.py
+++ e-commerce project/Blog/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from Blog.api.serializers import BlogSerializers
 from Blog.models import Blog
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def blog_api_view(request):
     blog = Blog.objects.all()
     serializer = BlogSerializers(blog, many=True)
